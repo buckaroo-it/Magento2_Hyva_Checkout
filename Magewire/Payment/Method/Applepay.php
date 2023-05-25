0c55aed842d3c72c70244ea733d0e2a6994e0321
Ivascu Madalin <madalin.ivascu@arnia.ro>
2023-05-25 07:44:38 +0000

    Added applepay

---
+++ Magewire/Payment/Method/Applepay.php
@@ -0,0 +1,194 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Buckaroo\HyvaCheckout\Magewire\Payment\Method;
+
+use Rakit\Validation\Validator;
+use Magewirephp\Magewire\Component;
+use Magento\Framework\View\Asset\Repository;
+use Magento\Quote\Api\CartRepositoryInterface;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Checkout\Model\Session as SessionCheckout;
+use Hyva\Checkout\Model\Magewire\Component\EvaluationInterface;
+use Hyva\Checkout\Model\Magewire\Component\EvaluationResultFactory;
+use Hyva\Checkout\Model\Magewire\Component\EvaluationResultInterface;
+use Buckaroo\Magento2\Model\ConfigProvider\Method\Applepay as MethodConfigProvider;
+use Magento\Quote\Model\Quote;
+
+class Applepay extends Component\Form implements EvaluationInterface
+{
+    protected $listeners = [
+        'shipping_method_selected' => 'refresh',
+        'payment_method_selected' => 'refresh',
+        'coupon_code_applied' => 'refresh',
+        'coupon_code_revoked' => 'refresh'
+    ];
+
+    public ?string $encriptedData = null;
+
+    public array $config = [];
+
+    public array $totals = [];
+
+    public array $grandTotal = [];
+
+    protected SessionCheckout $sessionCheckout;
+
+    protected CartRepositoryInterface $quoteRepository;
+
+    protected MethodConfigProvider $methodConfigProvider;
+
+    protected Repository $assetRepo;
+
+    public function __construct(
+        Validator $validator,
+        SessionCheckout $sessionCheckout,
+        CartRepositoryInterface $quoteRepository,
+        MethodConfigProvider $methodConfigProvider,
+        Repository $assetRepo
+    ) {
+        parent::__construct($validator);
+
+        $this->sessionCheckout = $sessionCheckout;
+        $this->quoteRepository = $quoteRepository;
+        $this->methodConfigProvider = $methodConfigProvider;
+        $this->assetRepo = $assetRepo;
+    }
+
+    public function mount(): void
+    {
+        $this->config = $this->getJsonConfig();
+        $this->totals = $this->getTotalLines();
+        $this->grandTotal = $this->getGrandTotal();
+    }
+
+    public function hydrate()
+    {
+        $this->config = $this->getJsonConfig();
+        $this->totals = $this->getTotalLines();
+        $this->grandTotal = $this->getGrandTotal();
+    }
+
+    public function updateData(string $paymentData, string $billingContact)
+    {
+        try {
+            $quote = $this->sessionCheckout->getQuote();
+            $quote->getPayment()->setAdditionalInformation('applepayTransaction', $paymentData);
+            $quote->getPayment()->setAdditionalInformation('billingContact', $billingContact);
+
+            $this->quoteRepository->save($quote);
+        } catch (LocalizedException $exception) {
+            $this->dispatchErrorMessage($exception->getMessage());
+        }
+    }
+    public function evaluateCompletion(EvaluationResultFactory $resultFactory): EvaluationResultInterface
+    {
+        try {
+            $quote = $this->sessionCheckout->getQuote();
+            $paymentData = $quote->getPayment()->getAdditionalInformation('applepayTransaction');
+
+            if (empty($paymentData)) {
+                return $resultFactory->createErrorMessageEvent()
+                    ->withCustomEvent('payment:method:error')
+                    ->withMessage('Payment data is missing');
+            }
+        } catch (LocalizedException $exception) {
+            $this->dispatchErrorMessage($exception->getMessage());
+        }
+
+       
+
+        return $resultFactory->createSuccess();
+    }
+
+    public function getJsSdkUrl(): string
+    {
+        try {
+            return $this->assetRepo->getUrl('Buckaroo_HyvaCheckout::js/applepay.js');
+        } catch (LocalizedException $exception) {
+            $this->dispatchErrorMessage($exception->getMessage());
+        }
+    }
+
+
+    private function getJsonConfig(): array
+    {
+
+        $config = $this->methodConfigProvider->getConfig();
+        if(!isset($config['payment']['buckaroo']['applepay'])) {
+            $this->dispatchErrorMessage('Cannot retrieved config');
+        }
+        return $config['payment']['buckaroo']['applepay'];
+    }
+
+    /**
+     * Get list of totals
+     *
+     * @return array
+     */
+    private function getTotalLines(): array
+    {
+        $totals = [];
+        $quote = $this->getQuote();
+        if($quote === null) {
+            return $totals;
+        }
+        $quote->collectTotals();
+        foreach ($quote->getTotals() as $key => $total) {
+            if($total->getData('value') != 0 && $key !== 'grand_total') {
+                $amount = $total->getData('value');
+                if($key === 'subtotal') {
+                    $amount = $quote->getSubtotalWithDiscount();//for subtotal we get it with discounts
+                }
+
+                $totals[] = [
+                    "label" => $total->getData('title'),
+                    "amount" => $amount,
+                    "type" => 'final',
+                ];
+            }
+        }
+        
+        return $totals;
+    }
+
+    /**
+     * Get grand total
+     *
+     * @return array
+     */
+    private function getGrandTotal(): array
+    {
+        $quote = $this->getQuote();
+        if($quote === null) {
+            return [];
+        }
+        if(!isset($quote->getTotals()['grand_total'])) {
+            return [];
+        }
+
+        $total = $quote->getTotals()['grand_total'];
+
+        return [
+            "label" => $total->getData('title'),
+            "amount" => $total->getData('value'),
+            "type" => 'final',
+        ];
+    }
+
+    /**
+     * Get quote fro session
+     *
+     * @return Quote|null
+     */
+    private function getQuote() :?Quote
+    {
+        try {
+           return $this->sessionCheckout->getQuote();
+        } catch (LocalizedException $exception) {
+            $this->dispatchErrorMessage($exception->getMessage());
+        }
+        return null;
+    }
+}
--- Magewire/Payment/Method/Voucher.php
+++ Magewire/Payment/Method/Voucher.php
@@ -6,6 +6,8 @@ namespace Buckaroo\HyvaCheckout\Magewire\Payment\Method;
 
 use Magento\Framework\UrlInterface;
 use Magewirephp\Magewire\Component;
+use Buckaroo\Magento2\Helper\PaymentGroupTransaction;
+use Magento\Checkout\Model\Session as SessionCheckout;
 use Hyva\Checkout\Model\Magewire\Component\EvaluationInterface;
 use Hyva\Checkout\Model\Magewire\Component\EvaluationResultFactory;
 use Hyva\Checkout\Model\Magewire\Component\EvaluationResultInterface;
@@ -17,8 +19,30 @@ class Voucher extends Component\Form implements EvaluationInterface
 
     protected UrlInterface $urlBuilder;
 
-    public function __construct(UrlInterface $urlBuilder) {
+    protected SessionCheckout $sessionCheckout;
+
+    protected PaymentGroupTransaction $groupTransaction;
+
+    public function __construct(
+        UrlInterface $urlBuilder,
+        SessionCheckout $sessionCheckout,
+        PaymentGroupTransaction $groupTransaction
+    ) {
         $this->urlBuilder = $urlBuilder;
+        $this->sessionCheckout = $sessionCheckout;
+        $this->groupTransaction = $groupTransaction;
+    }
+
+    /**
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
+     */
+    public function mount(): void
+    {
+        $quote = $this->sessionCheckout->getQuote();
+        $this->canSubmit = abs(
+            $this->groupTransaction->getAlreadyPaid($quote->getReservedOrderId()) - round($quote->getGrandTotal(), 2)
+        ) < 0.05;
     }
 
     public function evaluateCompletion(EvaluationResultFactory $resultFactory): EvaluationResultInterface
@@ -34,7 +58,7 @@ class Voucher extends Component\Form implements EvaluationInterface
 
     public function getAjaxUrl()
     {
-        return $this->urlBuilder->getRouteUrl('rest/default/V1/buckaroo/voucher/')."apply";
+        return $this->urlBuilder->getRouteUrl('rest/default/V1/buckaroo/voucher/') . "apply";
     }
 
     public function setCanSubmit(bool $canSubmit): void
