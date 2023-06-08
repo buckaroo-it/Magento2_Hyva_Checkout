8754dbdad572fe3aaf39b74eb5260b7efe064e5c
Ivascu Madalin <madalin.ivascu@arnia.ro>
2023-06-08 09:23:39 +0000

    Correctly display fee

---
+++ Block/Totals/Fee.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Buckaroo\HyvaCheckout\Block\Totals;
+
+use Magento\Checkout\Model\Session as SessionCheckout;
+use Buckaroo\Magento2\Helper\PaymentFee;
+use Magento\Framework\View\Element\Template\Context;
+
+class Fee extends \Magento\Framework\View\Element\Template
+{
+    protected PaymentFee $feeHelper;
+
+    protected  SessionCheckout $sessionCheckout;
+
+    public function __construct(
+        Context $context,
+        array $data,
+        PaymentFee $feeHelper,
+        SessionCheckout $sessionCheckout
+    ) {
+        parent::__construct($context, $data);
+        $this->feeHelper = $feeHelper;
+        $this->sessionCheckout = $sessionCheckout;
+    }
+    /**
+     * Get title based on payment method config
+     *
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        try {
+            $payment = $this->sessionCheckout
+            ->getQuote()
+            ->getPayment();
+            return $this->feeHelper->getBuckarooPaymentFeeLabel($payment->getMethod());
+        } catch (\Throwable $th) {
+            return __('Fee');
+        }
+        return __('Fee');
+    }
+
+    /**
+     * Get total from array of data
+     *
+     * @return float
+     */
+    public function getTotal(): float
+    {
+        $totalData = $this->getSegment();
+        if (
+            isset($totalData['extension_attributes']['buckaroo_fee']) &&
+            is_scalar($totalData['extension_attributes']['buckaroo_fee'])
+        ) {
+            return floatval($totalData['extension_attributes']['buckaroo_fee']);
+        }
+        return 0;
+    }
+}
