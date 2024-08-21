634cdaae6aa2f1f682d30573bf1b46372811aef2
a.baraliu <baraliualbina@gmail.com>
2024-08-21 17:05:48 +0200

    Revert "BP-3546-Use-object-as-array-in-Buckaroo-HyvaCheckout-Block-Totals-Fee-getTotal-22"
    This reverts commit b3f8499eb9e74874c509d202d2713b15158540dc.

---
+++ Block/Totals/Fee.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Buckaroo\HyvaCheckout\Block\Totals;
+
+use Magento\Checkout\Model\Session as SessionCheckout;
+use Buckaroo\Magento2\Helper\PaymentFee;
+use Magento\Framework\View\Element\Template\Context;
+
+class Fee extends \Magento\Framework\View\Element\Template
+{
+    protected PaymentFee $feeHelper;
+
+    protected SessionCheckout $sessionCheckout;
+
+    public function __construct(
+        Context         $context,
+        array           $data,
+        PaymentFee      $feeHelper,
+        SessionCheckout $sessionCheckout
+    )
+    {
+        parent::__construct($context, $data);
+        $this->feeHelper = $feeHelper;
+        $this->sessionCheckout = $sessionCheckout;
+    }
+
+    /**
+     * Get title based on payment method config
+     *
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        try {
+            $payment = $this->sessionCheckout
+                ->getQuote()
+                ->getPayment();
+            return $this->feeHelper->getBuckarooPaymentFeeLabel($payment->getMethod());
+        } catch (\Throwable $th) {
+            return __('Fee');
+        }
+        return __('Fee');
+    }
+
+    /**
+     * Get total from array of data
+     *
+     * @return float
+     */
+    public function getTotal(): float
+    {
+        $totalData = $this->getSegment();
+        $extensionAttributes = $totalData['extension_attributes'] ?? null;
+        if (
+            $extensionAttributes
+        ) {
+            return (floatval($extensionAttributes->getBuckarooFee()['buckaroo_fee'][0]));
+        }
+        return 0;
+    }
+}
