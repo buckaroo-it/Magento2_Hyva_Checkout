1cabf45c6143b03349e91515979bc0f850040c0f
a.baraliu <baraliualbina@gmail.com>
2024-12-04 09:50:51 +0100

    Add observer for applepay

--- view/frontend/web/js/hide-applepay.js
+++ view/frontend/web/js/hide-applepay.js
@@ -1,13 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const checkPaySupport = function () {
-        if (!('ApplePaySession' in window)) return false;
-        if (typeof ApplePaySession === 'undefined') return false;
-        return true;
+document.addEventListener('DOMContentLoaded', function () {
+    const hideApplePayIfUnsupported = function () {
+        const checkApplePaySupport = function () {
+            if (!('ApplePaySession' in window)) return false;
+            if (typeof ApplePaySession === 'undefined') return false;
+            return true;
+        };
+
+        const applePaySupported = checkApplePaySupport();
+        const applePayContainer = document.getElementById('payment-method-option-buckaroo_magento2_applepay');
+        if (!applePaySupported && applePayContainer) {
+            applePayContainer.style.display = 'none';
+        }
     };
 
-    const applePaySupported = checkPaySupport();
-    const applePayContainer = document.getElementById('payment-method-option-buckaroo_magento2_applepay');
-    if (!applePaySupported && applePayContainer) {
-        applePayContainer.style.display = 'none';
+    hideApplePayIfUnsupported();
+
+    const observer = new MutationObserver(() => {
+        hideApplePayIfUnsupported();
+    });
+
+    if (document.body) {
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    } else {
+        console.error("document.body is not available for observation.");
     }
 });
